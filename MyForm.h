0c45887c8eaa3d80569a2352b0f93c958b3d0dca
KIMBIEW <jitkhon1979@gmail.com>
2025-12-22 15:43:08 +0700

    test

--- MyForm.h
+++ MyForm.h
@@ -62,6 +62,7 @@ namespace ConsoleApplication3 {
 	private: System::Windows::Forms::ToolStripButton^ toolStripButton2;
 	private: System::Windows::Forms::ToolStripButton^ toolStripButton3;
 	private: System::Windows::Forms::ToolStripMenuItem^ uploadToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ kkkToolStripMenuItem;
 
 	private:
 		Bitmap^ bmp;
@@ -92,13 +93,14 @@ namespace ConsoleApplication3 {
 			this->toolStripMenuItem6 = (gcnew System::Windows::Forms::ToolStripSeparator());
 			this->convertToHSVToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->toolStripMenuItem5 = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->uploadToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->toolStrip1 = (gcnew System::Windows::Forms::ToolStrip());
 			this->toolStripButton1 = (gcnew System::Windows::Forms::ToolStripButton());
 			this->toolStripButton2 = (gcnew System::Windows::Forms::ToolStripButton());
 			this->toolStripButton3 = (gcnew System::Windows::Forms::ToolStripButton());
 			this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->saveFileDialog = (gcnew System::Windows::Forms::SaveFileDialog());
-			this->uploadToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->kkkToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->toolStripContainer1->ContentPanel->SuspendLayout();
 			this->toolStripContainer1->TopToolStripPanel->SuspendLayout();
 			this->toolStripContainer1->SuspendLayout();
@@ -113,28 +115,25 @@ namespace ConsoleApplication3 {
 			// toolStripContainer1.ContentPanel
 			// 
 			this->toolStripContainer1->ContentPanel->Controls->Add(this->pictureBox1);
-			this->toolStripContainer1->ContentPanel->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->toolStripContainer1->ContentPanel->Size = System::Drawing::Size(1924, 904);
+			this->toolStripContainer1->ContentPanel->Size = System::Drawing::Size(1443, 728);
 			this->toolStripContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->toolStripContainer1->Location = System::Drawing::Point(0, 0);
-			this->toolStripContainer1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->toolStripContainer1->Name = L"toolStripContainer1";
-			this->toolStripContainer1->Size = System::Drawing::Size(1924, 959);
+			this->toolStripContainer1->Size = System::Drawing::Size(1443, 779);
 			this->toolStripContainer1->TabIndex = 0;
 			this->toolStripContainer1->Text = L"toolStripContainer1";
 			// 
 			// toolStripContainer1.TopToolStripPanel
 			// 
-			this->toolStripContainer1->TopToolStripPanel->Controls->Add(this->menuStrip1);
 			this->toolStripContainer1->TopToolStripPanel->Controls->Add(this->toolStrip1);
+			this->toolStripContainer1->TopToolStripPanel->Controls->Add(this->menuStrip1);
 			// 
 			// pictureBox1
 			// 
 			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pictureBox1->Location = System::Drawing::Point(0, 0);
-			this->pictureBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(1924, 904);
+			this->pictureBox1->Size = System::Drawing::Size(1443, 728);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
@@ -143,13 +142,13 @@ namespace ConsoleApplication3 {
 			// 
 			this->menuStrip1->Dock = System::Windows::Forms::DockStyle::None;
 			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
 				this->toolStripMenuItem1,
-					this->imageToolStripMenuItem, this->uploadToolStripMenuItem
+					this->imageToolStripMenuItem, this->uploadToolStripMenuItem, this->kkkToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 27);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1924, 28);
+			this->menuStrip1->Size = System::Drawing::Size(1443, 24);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -160,52 +159,52 @@ namespace ConsoleApplication3 {
 					this->toolStripMenuItem3, this->saveFileMenu, this->toolStripMenuItem7, this->saveAsFileMenu, this->toolStripMenuItem8, this->exitToolStripMenuItem
 			});
 			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
-			this->toolStripMenuItem1->Size = System::Drawing::Size(46, 24);
+			this->toolStripMenuItem1->Size = System::Drawing::Size(37, 20);
 			this->toolStripMenuItem1->Text = L"&File";
 			// 
 			// toolStripMenuItem2
 			// 
 			this->toolStripMenuItem2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripMenuItem2.Image")));
 			this->toolStripMenuItem2->Name = L"toolStripMenuItem2";
-			this->toolStripMenuItem2->Size = System::Drawing::Size(224, 26);
+			this->toolStripMenuItem2->Size = System::Drawing::Size(184, 26);
 			this->toolStripMenuItem2->Text = L"&Open";
 			this->toolStripMenuItem2->Click += gcnew System::EventHandler(this, &MyForm::toolStripMenuItem2_Click);
 			// 
 			// toolStripMenuItem3
 			// 
 			this->toolStripMenuItem3->Name = L"toolStripMenuItem3";
-			this->toolStripMenuItem3->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem3->Size = System::Drawing::Size(181, 6);
 			// 
 			// saveFileMenu
 			// 
 			this->saveFileMenu->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"saveFileMenu.Image")));
 			this->saveFileMenu->Name = L"saveFileMenu";
-			this->saveFileMenu->Size = System::Drawing::Size(224, 26);
+			this->saveFileMenu->Size = System::Drawing::Size(184, 26);
 			this->saveFileMenu->Text = L"&save";
 			this->saveFileMenu->Click += gcnew System::EventHandler(this, &MyForm::saveFileMenu_Click);
 			// 
 			// toolStripMenuItem7
 			// 
 			this->toolStripMenuItem7->Name = L"toolStripMenuItem7";
-			this->toolStripMenuItem7->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem7->Size = System::Drawing::Size(181, 6);
 			// 
 			// saveAsFileMenu
 			// 
 			this->saveAsFileMenu->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"saveAsFileMenu.Image")));
 			this->saveAsFileMenu->Name = L"saveAsFileMenu";
-			this->saveAsFileMenu->Size = System::Drawing::Size(224, 26);
+			this->saveAsFileMenu->Size = System::Drawing::Size(184, 26);
 			this->saveAsFileMenu->Text = L"&Save &As";
 			this->saveAsFileMenu->Click += gcnew System::EventHandler(this, &MyForm::saveAsFileMenu_Click);
 			// 
 			// toolStripMenuItem8
 			// 
 			this->toolStripMenuItem8->Name = L"toolStripMenuItem8";
-			this->toolStripMenuItem8->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem8->Size = System::Drawing::Size(181, 6);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
-			this->exitToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->exitToolStripMenuItem->Size = System::Drawing::Size(184, 26);
 			this->exitToolStripMenuItem->Text = L"&Exit";
 			this->exitToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::exitToolStripMenuItem_Click);
 			// 
@@ -216,45 +215,52 @@ namespace ConsoleApplication3 {
 					this->toolStripMenuItem4, this->drawCircleToolStripMenuItem, this->toolStripMenuItem6, this->convertToHSVToolStripMenuItem, this->toolStripMenuItem5
 			});
 			this->imageToolStripMenuItem->Name = L"imageToolStripMenuItem";
-			this->imageToolStripMenuItem->Size = System::Drawing::Size(65, 24);
+			this->imageToolStripMenuItem->Size = System::Drawing::Size(52, 20);
 			this->imageToolStripMenuItem->Text = L"&Image";
 			this->imageToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::imageToolStripMenuItem_Click);
 			// 
 			// drawlineToolStripMenuItem
 			// 
 			this->drawlineToolStripMenuItem->Name = L"drawlineToolStripMenuItem";
-			this->drawlineToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->drawlineToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->drawlineToolStripMenuItem->Text = L"Draw &line";
 			this->drawlineToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::drawlineToolStripMenuItem_Click);
 			// 
 			// toolStripMenuItem4
 			// 
 			this->toolStripMenuItem4->Name = L"toolStripMenuItem4";
-			this->toolStripMenuItem4->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem4->Size = System::Drawing::Size(177, 6);
 			// 
 			// drawCircleToolStripMenuItem
 			// 
 			this->drawCircleToolStripMenuItem->Name = L"drawCircleToolStripMenuItem";
-			this->drawCircleToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->drawCircleToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->drawCircleToolStripMenuItem->Text = L"draw &Circle";
 			this->drawCircleToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::drawCircleToolStripMenuItem_Click);
 			// 
 			// toolStripMenuItem6
 			// 
 			this->toolStripMenuItem6->Name = L"toolStripMenuItem6";
-			this->toolStripMenuItem6->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem6->Size = System::Drawing::Size(177, 6);
 			// 
 			// convertToHSVToolStripMenuItem
 			// 
 			this->convertToHSVToolStripMenuItem->Name = L"convertToHSVToolStripMenuItem";
-			this->convertToHSVToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+			this->convertToHSVToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->convertToHSVToolStripMenuItem->Text = L"Convert to HSV";
 			this->convertToHSVToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::convertToHSVToolStripMenuItem_Click);
 			// 
 			// toolStripMenuItem5
 			// 
 			this->toolStripMenuItem5->Name = L"toolStripMenuItem5";
-			this->toolStripMenuItem5->Size = System::Drawing::Size(221, 6);
+			this->toolStripMenuItem5->Size = System::Drawing::Size(177, 6);
+			// 
+			// uploadToolStripMenuItem
+			// 
+			this->uploadToolStripMenuItem->Name = L"uploadToolStripMenuItem";
+			this->uploadToolStripMenuItem->Size = System::Drawing::Size(56, 20);
+			this->uploadToolStripMenuItem->Text = L"upload";
+			this->uploadToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::uploadToolStripMenuItem_Click);
 			// 
 			// toolStrip1
 			// 
@@ -266,7 +272,7 @@ namespace ConsoleApplication3 {
 			});
 			this->toolStrip1->Location = System::Drawing::Point(4, 0);
 			this->toolStrip1->Name = L"toolStrip1";
-			this->toolStrip1->Size = System::Drawing::Size(100, 27);
+			this->toolStrip1->Size = System::Drawing::Size(84, 27);
 			this->toolStrip1->TabIndex = 1;
 			// 
 			// toolStripButton1
@@ -275,7 +281,7 @@ namespace ConsoleApplication3 {
 			this->toolStripButton1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton1.Image")));
 			this->toolStripButton1->ImageTransparentColor = System::Drawing::Color::Magenta;
 			this->toolStripButton1->Name = L"toolStripButton1";
-			this->toolStripButton1->Size = System::Drawing::Size(29, 24);
+			this->toolStripButton1->Size = System::Drawing::Size(24, 24);
 			this->toolStripButton1->Text = L"toolStripButton1";
 			this->toolStripButton1->Click += gcnew System::EventHandler(this, &MyForm::toolStripButton1_Click);
 			// 
@@ -285,7 +291,7 @@ namespace ConsoleApplication3 {
 			this->toolStripButton2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton2.Image")));
 			this->toolStripButton2->ImageTransparentColor = System::Drawing::Color::Magenta;
 			this->toolStripButton2->Name = L"toolStripButton2";
-			this->toolStripButton2->Size = System::Drawing::Size(29, 24);
+			this->toolStripButton2->Size = System::Drawing::Size(24, 24);
 			this->toolStripButton2->Text = L"toolStripButton2";
 			this->toolStripButton2->Click += gcnew System::EventHandler(this, &MyForm::toolStripButton2_Click);
 			// 
@@ -295,7 +301,7 @@ namespace ConsoleApplication3 {
 			this->toolStripButton3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton3.Image")));
 			this->toolStripButton3->ImageTransparentColor = System::Drawing::Color::Magenta;
 			this->toolStripButton3->Name = L"toolStripButton3";
-			this->toolStripButton3->Size = System::Drawing::Size(29, 24);
+			this->toolStripButton3->Size = System::Drawing::Size(24, 24);
 			this->toolStripButton3->Text = L"toolStripButton3";
 			this->toolStripButton3->Click += gcnew System::EventHandler(this, &MyForm::toolStripButton3_Click);
 			// 
@@ -309,21 +315,19 @@ namespace ConsoleApplication3 {
 			this->saveFileDialog->DefaultExt = L"png";
 			this->saveFileDialog->Filter = L"Image files | *.jpg; *.png";
 			// 
-			// uploadToolStripMenuItem
+			// kkkToolStripMenuItem
 			// 
-			this->uploadToolStripMenuItem->Name = L"uploadToolStripMenuItem";
-			this->uploadToolStripMenuItem->Size = System::Drawing::Size(70, 24);
-			this->uploadToolStripMenuItem->Text = L"upload";
-			this->uploadToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::uploadToolStripMenuItem_Click);
+			this->kkkToolStripMenuItem->Name = L"kkkToolStripMenuItem";
+			this->kkkToolStripMenuItem->Size = System::Drawing::Size(37, 20);
+			this->kkkToolStripMenuItem->Text = L"kkk";
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1924, 959);
+			this->ClientSize = System::Drawing::Size(1443, 779);
 			this->Controls->Add(this->toolStripContainer1);
 			this->MainMenuStrip = this->menuStrip1;
-			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"MyForm";
 			this->Text = L"My Image Viewer";
 			this->toolStripContainer1->ContentPanel->ResumeLayout(false);
